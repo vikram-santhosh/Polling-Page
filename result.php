42ce54f18ec1af9c4bfeb098d79c0e5613b9e3c3
vikram <vikramsanthosh1@gmail.com>
2015-07-26 14:50:08 +0530

    Bar Chart

--- result.php
+++ result.php
@@ -9,75 +9,56 @@
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-    <script scr="/Chart.js-master/Chart.js"></script>
 </head>
 <body>
-        
-    <h1> Results</h1>
-    <canvas id="tourist" width="300" height="400"> Chart </canvas>
-
+    <div class = "jumbotron"> 
+        <h1> Results</h1>
+    </div>
+    <div class = "container" >
     
-    <?php 
-        require 'db_connect.inc.php';
-        $place = array("agra","varanasi","lek","hampi","darjeeling","goa");
-        $count = array();
-        for($i=0;$i<6;$i++){
-            $temp = $place[$i];
-            $query_val = "SELECT $temp FROM poll";
-            if($query_val_run = mysql_query($query_val)){
-                $y = mysql_result($query_val_run,0,$place[$i]);
-                array_push($count,$y);
-            }
-        }   
-       // print_r($count);
-    ?>
+    <div class = "text-center"> 
+            <?php 
+                require 'db_connect.inc.php';
+                $place = array("agra","varanasi","lek","hampi","darjeeling","goa");
+                $count = array();
+                for($i=0;$i<6;$i++){
+                    $temp = $place[$i];
+                    $query_val = "SELECT $temp FROM poll";
+                    if($query_val_run = mysql_query($query_val)){
+                        $y = mysql_result($query_val_run,0,$place[$i]);
+                        array_push($count,$y);
+                    }
+                }   
+            ?>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
-    <script type="text/javascript" >
-                
-        var context = document.getElementById('tourist').getContext('2d');
-        
-        var val = <?php echo json_encode($count) ?>;
-            for( i=0;i<6;i++ )
-                document.write(val[i]);
-        var chart_data =    [
-                                
-                                {
-                                    lable:'Agra',
-                                    value:val[0],
-                                    color:'#FF0000'
-                                },
-                                {
-                                    lable:'Varanasi',
-                                    value:val[1],
-                                    color:'#003399'
-                                },
-                                {
-                                    lable:'Ledak',
-                                    value:val[2],
-                                    color:'#006600'
-                                },
-                                {
-                                    lable:'Hampi',
-                                    value:val[3],
-                                    color:'#996633'
-                                },
-                                {
-                                    lable:'Darjeeling',
-                                    value:val[4],
-                                    color:'#FFCC00'
-                                },
-                                {
-                                    lable:'Goa',
-                                    value:val[5],
-                                    color:'#660066'
-                                }
-        
-                            ];
-        
-        var my_chart = new Chart(context).Pie(chart_data);
-        document.write("heree");
-    </script>
+            <canvas id="polling_chart" width="500" height="500" class = "center"></canvas>
 
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
+
+            <script type="text/javascript" >
+            var val = <?php echo json_encode($count) ?>;
+
+
+                var ctx = document.getElementById("polling_chart").getContext("2d");
+                var data =  {
+                                labels: ["Agra", "Varanasi", "Ladakh", "Hampi", "Darjeeling", "Goa"],
+                                datasets: [
+                                    {
+                                        label: "My First dataset",
+                                        fillColor: "rgba(220,220,220,0.5)",
+                                        strokeColor: "rgba(220,220,220,0.8)",
+                                        highlightFill: "rgba(220,220,220,0.75)",
+                                        highlightStroke: "rgba(220,220,220,1)",
+                                        data: [ val[0],val[1],val[2],val[3],val[4],val[5] ]
+                                    }
+                                ]
+                            };
+
+                var pollingChart = new Chart(ctx).Bar(data);
+
+            </script>
+
+        </div>
+    </div>
 </body>
 </html>
